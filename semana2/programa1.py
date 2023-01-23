61e317698ffb79e0a9d660028f360f3fa5f46fa0
Jesús Raúl <85081862+jesusraulrg@users.noreply.github.com>
2023-01-23 16:58:22 +0000

    AGREGAMOS la carpeta semana2 y dentro de esta el programa1.py

---
+++ semana2/programa1.py
@@ -0,0 +1,10 @@
+"""
+  Programa1
+  Nombre: Jesús Raúl RG
+  Fecha: 23/01/2023
+  Descripciñon: comentarios multilinea, comentarios de una linea, concatenacion de cadena y cadenas de caracterd
+  
+"""
+variable1 = "Hola" #Variable que almacena una cadena de caracteres
+variable2 = "Python" #Variable que almacena una cadena de caracteres
+print (variable1 + variable2) #Concatena 2 cadenas
