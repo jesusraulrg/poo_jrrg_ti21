cace0168c5eb6d09e9008994cf76348a6a2a7da7
Jesús Raúl <85081862+jesusraulrg@users.noreply.github.com>
2023-01-24 19:58:29 +0000

    CONCATENAMOS utilizando str.format

--- semana2/programa1.py
+++ semana2/programa1.py
@@ -5,7 +5,7 @@
   Descripción: comentarios multilinea, comentarios de una linea, concatenacion de cadena y cadenas de caracteres
 """
 variable1 = "Hola" #Variable que almacena una cadena de caracteres ()
-variable2 = '619' #Variable que almacena una cadena de caracteres
+variable2 = "Python" #Variable que almacena una cadena de caracteres
 print (variable1 + variable2) #Concatena 2 cadenas
 
 
@@ -14,8 +14,8 @@ print (variable1 + variable2) #Concatena 2 cadenas
   Programa1
   Nombre: Jesús Raúl Rosas Gómez
   Fecha: 24/01/2023
-  Descripción: comentarios multilinea, comentarios de una linea, concatenacion de cadena y cadenas de caracterd 
+  Descripción: comentarios multilinea, comentarios de una linea, concatenacion de cadena y cadenas de caracteres 
 """
-variable1 = "Hola" #Variable que almacena una cadena de caracteres ()
-variable2 = '619' #Variable que almacena una cadena de caracteres
+variable1 = "Hola" #Variable tipo str
+variable2 = '619' #Variable tipo int
 print (variable1 + variable2) #Concatena 2 cadenas
---
+++ semana2/programa2.py
@@ -0,0 +1,19 @@
+"""
+  Programa2
+  Nombre: Jesús Raúl Rosas Gómez
+  Fecha: 24/01/2023
+  Descripción: utilizar comando str.format
+"""
+variable1 = "Hola" #Variable tipo str
+variable2 = '619' #Variable tipo int
+print ("{}{}" .format(variable1, variable2)) #.format nos permite imprimir valores sin importar el tipo de dato que estemos utilizando
+
+"""
+  Programa2
+  Nombre: Jesús Raúl Rosas Gómez
+  Fecha: 24/01/2023
+  Descripción: utilizar comando str.format
+"""
+variable1 = "Hola" #Variable tipo str
+variable2 = 619 #Variable tipo int
+print ("{}{}".format(variable1,variable2)) #.format nos permite imprimir valores sin importar el tipo de dato que estemos utilizando
