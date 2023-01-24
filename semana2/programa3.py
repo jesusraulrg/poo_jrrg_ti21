12ca474a04cb2cac9adda01c3d3dada380aef5c8
Jesús Raúl <85081862+jesusraulrg@users.noreply.github.com>
2023-01-24 20:52:09 +0000

    CALCULAMOS operaciones aritmeticas con sttr.format

---
+++ semana2/programa3.py
@@ -0,0 +1,14 @@
+"""
+  Programa2
+  Nombre: Jesús Raúl Rosas Gómez
+  Fecha: 24/01/2023
+  Descripción: print y operaciones aritméticas con .format
+"""
+numero1 = 10 #Variable tipo int
+numero2 = 5 #Variable tipo in
+resultado=numero1+numero2
+print (numero1+numero2) #15
+print ("{}+{}".format(numero1,numero2)) #10+5
+print ("{}+{}={}".format(numero1,numero2,numero1+numero2)) #10+5=15
+print ("{}={}*{}".format(numero1*numero2,numero1,numero2))
+print ("El resultado de dividir {}/{} es {}".format(numero1,numero2,numero1/numero2))
