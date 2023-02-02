1e8f95d86f95a29b680c382b7589c451b4f8e094
Jesús Raúl <85081862+jesusraulrg@users.noreply.github.com>
2023-02-02 20:32:03 +0000

    AGREGUE nuevos programas por completar

---
+++ semana3/programa5.py
@@ -0,0 +1,13 @@
+num1=int(input("Ingrese el primer número: "))
+num2=10
+result=num1+num2
+print("La suma de ", num1, "y", num2, "es", result)
+
+num1=int(input("Ingrese el primer numero: "))
+num2=20
+result=num1-num2
+print("La resta de ",num1,"y",num2,"es",result)
+
+numero1=int(input("Ingrese numero: "))
+numero2=2
+result=numero1
