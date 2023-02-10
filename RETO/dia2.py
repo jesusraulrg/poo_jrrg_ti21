20a39faa1b4306e62f09ea135e481c1a149f6356
Jesús Raúl <jesusraulrg@hotmail.com>
2023-02-09 23:29:17 -0600

    Reto día 2

---
+++ RETO/dia2.py
@@ -0,0 +1,10 @@
+#Martes 07-02-23
+#@jesusraulrg
+
+#Programa para realizar la multiplicación de dos numeros enteros
+
+n1=(int(input("Ingresa el primer numero de la multiplicación: ")))
+n2=(int(input("Ingresa el segundo numero de la multiplicación: ")))
+
+result=n1*n2
+print("El resultado de la multiplicación es: ",result)
